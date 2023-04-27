8d1f61846d5e4feddc3ed4330e6003bad81cb3ca
ankitraj5ar <ankitraj6241@gmail.com>
2023-04-27 12:20:48 +0530

    new path added for 418 response

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.get('/',(req,res) =>{
 	res.send('hello from node');
 
 });
+app.get('/headers',(req,res) =>{
+	res.set('X-full_stack','4life').status(418).send("i prefer horlicks");
 
+});
 app.listen(port,()=>{
 	console.log(`server started on port ${port} `);
 });
